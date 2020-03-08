25a238ebf9c62e82283687cbcd24dcc7343bf396
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-03-09 02:18:38 +0900

    section Two, Video Three

--- app/src/main/java/smartherd/com/MainActivity.kt
+++ app/src/main/java/smartherd/com/MainActivity.kt
@@ -1,5 +1,6 @@
 package smartherd.com
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -20,9 +21,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnSendMsgToNextActivity.setOnClickListener {
-            val etUserMessage
             val message: String = etUserMessage.text.toString()
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 
