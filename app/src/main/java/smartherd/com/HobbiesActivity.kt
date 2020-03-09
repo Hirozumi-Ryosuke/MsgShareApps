517a6b1bf1d8de6ab87ac42ce156403685ad132f
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-03-10 02:38:16 +0900

    CardView

--- app/src/main/java/smartherd/com/HobbiesActivity.kt
+++ app/src/main/java/smartherd/com/HobbiesActivity.kt
@@ -1,2 +1,13 @@
 package smartherd.com
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HobbiesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hobbies)
+    }
+    
+}
