3cf8f4ca31eec51d2737d2b730994e3cd06b40fa
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-03-10 22:54:20 +0900

    android text

--- app/src/main/java/smartherd/com/HobbiesActivity.kt
+++ app/src/main/java/smartherd/com/HobbiesActivity.kt
@@ -1,24 +1,29 @@
 package smartherd.com
 
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class HobbiesAdapter() : RecyclerView.Adapter<HobbiesAdapter.MyViewHolder>() {
+class HobbiesAdapter(val context: Context, val hobbies: List<Hobby>) : RecyclerView.Adapter<HobbiesAdapter.MyViewHolder>() {
 
-    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false)
+        return MyViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return hobbies.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         TODO("Not yet implemented")
     }
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
 }
